0211ea248e59038f29b2ff4b7ca52ca59b9c50d9
leeguooooo <guoli@zhihu.com>
2025-12-23 16:19:55 +0900

    Bump version to 3.43

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ elif (3, 0) < version < (3, 5):
           ' ({}.{} detected).'.format(*version))
     sys.exit(-1)
 
-VERSION = '3.42'
+VERSION = '3.43'
 
 install_requires = ['psutil', 'colorama', 'six', 'rich; python_version>="3.8"']
 extras_require = {':python_version<"3.4"': ['pathlib2'],
